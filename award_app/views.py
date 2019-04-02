76215211f36ff9f4b3e4e3dd4899974cd2fb9846
Berthedusabeyuzu <berthe1030@gmail.com>
2019-04-02 15:54:13 +0200

    i have done profile html

--- award_app/views.py
+++ award_app/views.py
@@ -30,9 +30,9 @@ def new_profile(request):
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = current_user
-            profile.save()    
+            profile.save()        
         return redirect('welcome')
-    
+       
     else:
         form = NewProfileForm()
     return render(request, 'new-profile.html', {"form": form})
